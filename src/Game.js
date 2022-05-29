f4ecafb46ac249c204f99d59af2b115ac57f0939
Michael Bashford <mikebashford@gmail.com>
2022-05-29 18:18:10 -0500

    Printing out player symbols to board

--- src/Game.js
+++ src/Game.js
@@ -1,25 +1,29 @@
 import React, { useState } from "react";
 import Board from "./Board";
 
-const board = ["", "", "", "", "", "", "", "", ""];
+const initialBoard = ["", "", "", "", "", "", "", "", ""];
 
 const PLAYER_1_SYMBOL = "X";
 const PLAYER_2_SYMBOL = "O";
 
 export default function Game() {
+  const [board, setBoard] = useState(initialBoard);
   const [currentPlayer, setCurrentPlayer] = useState(PLAYER_1_SYMBOL);
+
+  function handleSquareClick(index) {
+    if (!board[index]) {
+      const newBoard = [...board];
+      newBoard[index] = currentPlayer;
+      setBoard(newBoard);
+    }
+    setCurrentPlayer(
+      currentPlayer === PLAYER_1_SYMBOL ? PLAYER_2_SYMBOL : PLAYER_1_SYMBOL
+    );
+  }
+
   return (
     <div>
-      <Board
-        board={board}
-        onSquareClick={(index) =>
-          setCurrentPlayer(
-            currentPlayer === PLAYER_1_SYMBOL
-              ? PLAYER_2_SYMBOL
-              : PLAYER_1_SYMBOL
-          )
-        }
-      />
+      <Board board={board} onSquareClick={handleSquareClick} />
     </div>
   );
 }
